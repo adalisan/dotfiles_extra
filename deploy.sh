ae507260037fa95e1fc239e688d80151ab9353b5
strboul <25015317+strboul@users.noreply.github.com>
2018-10-21 22:30:11 +0200

    Functionalize prompt part in the deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,27 +9,37 @@ echo "Updating configuration"
 (cd ~/dotfiles && git pull && git submodule update --init --recursive)
 
 # Source: https://stackoverflow.com/a/27875395/
-prompt_install() {
-	echo -n "$1 is not installed. Would you like to install it? (y/N) " >&2
+user_prompt() {
+	echo "$@"
 	old_stty_cfg=$(stty -g)
 	stty raw -echo
 	answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
-	stty $old_stty_cfg && echo
+	stty "$old_stty_cfg"
 	if echo "$answer" | grep -iq "^y" ;then
-		if [ -x "$(command -v apt-get)" ]; then
-			sudo apt-get install $1 -y
-		elif [ -x "$(command -v brew)" ]; then
-			brew install $1
-		else
-			echo "platform/pkg manager not supported."
-		fi
+		echo "$answer"
+		: # do nothing. proceed to script.
+	else
+		echo "$answer"
+        	echo "Aborting."
+        	exit 0
+	fi
+}
+
+do_install() {
+	user_prompt "$1 is not installed. Would you like to install it? [y/N]" >&2
+	if [ -x "$(command -v apt-get)" ]; then
+		sudo apt-get install $1 -y
+	elif [ -x "$(command -v brew)" ]; then
+		brew install $1
+	else
+		echo "platform/pkg manager not supported."
 	fi
 }
 
 check_for_software() {
 	echo "Checking to see if $1 is installed"
 	if ! [ -x "$(command -v $1)" ]; then
-		prompt_install $1
+		do_install $1
 	else
 		echo "$1 is installed."
 	fi
@@ -41,19 +51,7 @@ echo "$dashes"
 echo "Configure and install (if not existed) the CLI essentials:"
 echo "zsh, nvim, tmux, and fzf"
 echo
-echo "Let's get started? (y/N)"
-old_stty_cfg=$(stty -g)
-stty raw -echo
-answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
-stty $old_stty_cfg
-if echo "$answer" | grep -iq "^y" ;then
-	echo "$answer"
-	echo
-else
-	echo "$answer"
-	echo "Aborting. No change has been made."
-	exit 0
-fi
+user_prompt "Let's get started? [y/N]"
 
 check_for_software zsh
 echo
@@ -61,7 +59,7 @@ check_for_software nvim
 echo
 check_for_software tmux
 echo
-check_for_software ripgrep # ubuntu 16.04 not yet supported
+# check_for_software ripgrep # ubuntu 16.04 not yet supported
 echo
 # check_for_software fzf (there is no official apt package)
 FZF="$(command -v fzf)"
@@ -76,22 +74,13 @@ echo "Press [ENTER] to complete linking."
 source "$HOME"/dotfiles/zsh/link.sh
 
 # change the default shell:
-# Source: https://github.com/Parth/dotfiles/blob/master/deploy
 check_default_shell() {
 	if [ -z "${SHELL##*zsh*}" ]; then
-			echo "Default shell is zsh."
+		echo "Default shell is zsh."
 	else
-		echo -n "Default shell is not zsh. Do you want to chsh -s \$(which zsh)? (y/N)"
-		old_stty_cfg=$(stty -g)
-		stty raw -echo
-		answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
-		stty $old_stty_cfg && echo
-		if echo "$answer" | grep -iq "^y" ;then
-			echo "$answer"
-			chsh -s $(which zsh)
-		else
-			echo "$answer"
-			echo "The configuration will not work properly without zsh."
+		user_prompt "Default shell is not zsh. Do you want to chsh -s \$(which zsh)? (y/N)"
+		chsh -s $(which zsh)
+		# The configuration will not work properly without zsh.
 	fi
 }
 check_default_shell
